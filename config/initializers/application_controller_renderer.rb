6eaa87792e2d33dfd731ace83670c59a8b5433e3
takagi yuto <yto.oct.14@gmail.com>
2019-11-06 20:00:44 +0900

    [Finish]5-7

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'f8e218eb65d4a16b6822871e23fe1e35c96bea35175efe3e93e9b2d5da4b9aacea0d6075e7208b343aad183fea056a1c34767d572db2a85a39e82f06dd4ffb61'
---
+++ db/migrate/20191106082605_add_profile_image_id_tousers.rb
@@ -0,0 +1,5 @@
+class AddProfileImageIdTousers < ActiveRecord::Migration[5.2]
+  def change
+
+  end
+end
